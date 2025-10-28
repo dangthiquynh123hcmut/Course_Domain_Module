422912903b0099d5a24e9b929104a7acce572017
quynhdang <quynh.dangthi2212880@hcmut.edu.vn>
2025-10-28 17:16:09 +0700

    First commit

---
+++ src/main/java/course_domain/CourseDomainApplication.java
@@ -0,0 +1,13 @@
+package course_domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseDomainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseDomainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/course_domain/CourseDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package course_domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseDomainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
